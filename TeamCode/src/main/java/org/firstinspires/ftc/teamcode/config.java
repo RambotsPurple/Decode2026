635198b66441d30c19dec814e15d8af32d4904dd
Your Name <you@example.com>
2025-09-11 00:13:35 -0700

    updated config and a drivetrain subsystem

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/config.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/config.java
@@ -13,8 +13,7 @@ public class config {
 
 
 
-    public static DcMotorEx frontLeft = null, rearLeft = null;
-    public static DcMotorEx frontRight = null, rearRight = null;
+
 
 
 
@@ -41,27 +40,7 @@ public class config {
         );
 
         // Drive Train motor
-        frontLeft = hardwareMap.get(DcMotorEx.class, "leftFront");
-        frontRight = hardwareMap.get(DcMotorEx.class, "rightFront");
-        rearLeft = hardwareMap.get(DcMotorEx.class, "leftBack");
-        rearRight = hardwareMap.get(DcMotorEx.class, "rightBack");
 
-        frontLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        frontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        rearLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        rearRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-
-// DcMotors for Linear slide
-
-
-        // MaybeIntake = hardwareMap.get(DcMotorEx.class, "intake");
-        // Setting the direction for the motor on where to rotate
-
-        // Orientation for drivetrain
-        frontLeft.setDirection(DcMotor.Direction.FORWARD);
-        frontRight.setDirection(DcMotor.Direction.REVERSE);
-        rearLeft.setDirection(DcMotor.Direction.FORWARD);
-        rearRight.setDirection(DcMotor.Direction.REVERSE);
 
         // intake
 
@@ -102,38 +81,7 @@ public class config {
         return direction;
     }
 
-    public static void drive(double x, double y, double turn, double direction){
-        // input: theta and power
-        // theta is where we want the direction the robot to go
-        // power is (-1) to 1 scale where increasing power will cause the engines to go faster
-        double theta = Math.atan2(y, x) - Math.toRadians(direction);
-        double power = Math.hypot(x, y);
-        double sin = Math.sin(theta - Math.PI / 4);
-        double cos = Math.cos(theta - Math.PI / 4);
-        // max variable allows to use the motors at its max power with out disabling it
-        double max = Math.abs(Math.max(Math.abs(sin) + turn, Math.abs(cos) + turn));
-        max = Math.max(max, 1);
 
-        double frontLeftPower = power * cos / max + turn;
-        double frontRightPower = power * sin / max - turn;
-        double rearLeftPower = power * sin / max + turn;
-        double rearRightPower = power * cos / max - turn;
-
-        // Prevents the motors exceeding max power thus motors will not seize and act sporadically
-        if ((power + Math.abs(turn)) > 1) {
-            frontLeftPower /= power + turn;
-            frontRightPower /= power - turn;
-            rearLeftPower /= power + turn;
-            rearRightPower /= power - turn;
-        }
-
-        // Power to the wheels
-        frontLeft.setPower(frontLeftPower);
-        rearLeft.setPower(rearLeftPower);
-        frontRight.setPower(frontRightPower);
-        rearRight.setPower(rearRightPower);
-
-    }
 
 
 }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subSystems/driveTrain.java
@@ -0,0 +1,65 @@
+package org.firstinspires.ftc.teamcode.subSystems;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public class driveTrain {
+    private static DcMotorEx frontLeft, frontRight, rearLeft, rearRight ;
+
+    public  driveTrain (HardwareMap hw){
+        frontLeft = hw.get(DcMotorEx.class, "leftFront");
+        frontRight = hw.get(DcMotorEx.class, "rightFront");
+        rearLeft = hw.get(DcMotorEx.class, "leftBack");
+        rearRight = hw.get(DcMotorEx.class, "rightBack");
+
+        frontLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        frontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rearLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rearRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+// DcMotors for Linear slide
+
+
+        // MaybeIntake = hardwareMap.get(DcMotorEx.class, "intake");
+        // Setting the direction for the motor on where to rotate
+
+        // Orientation for drivetrain
+        frontLeft.setDirection(DcMotor.Direction.FORWARD);
+        frontRight.setDirection(DcMotor.Direction.REVERSE);
+        rearLeft.setDirection(DcMotor.Direction.FORWARD);
+        rearRight.setDirection(DcMotor.Direction.REVERSE);
+    }
+    public static void drive(double x, double y, double turn, double direction){
+        // input: theta and power
+        // theta is where we want the direction the robot to go
+        // power is (-1) to 1 scale where increasing power will cause the engines to go faster
+        double theta = Math.atan2(y, x) - Math.toRadians(direction);
+        double power = Math.hypot(x, y);
+        double sin = Math.sin(theta - Math.PI / 4);
+        double cos = Math.cos(theta - Math.PI / 4);
+        // max variable allows to use the motors at its max power with out disabling it
+        double max = Math.abs(Math.max(Math.abs(sin) + turn, Math.abs(cos) + turn));
+        max = Math.max(max, 1);
+
+        double frontLeftPower = power * cos / max + turn;
+        double frontRightPower = power * sin / max - turn;
+        double rearLeftPower = power * sin / max + turn;
+        double rearRightPower = power * cos / max - turn;
+
+        // Prevents the motors exceeding max power thus motors will not seize and act sporadically
+        if ((power + Math.abs(turn)) > 1) {
+            frontLeftPower /= power + turn;
+            frontRightPower /= power - turn;
+            rearLeftPower /= power + turn;
+            rearRightPower /= power - turn;
+        }
+
+        // Power to the wheels
+        frontLeft.setPower(frontLeftPower);
+        rearLeft.setPower(rearLeftPower);
+        frontRight.setPower(frontRightPower);
+        rearRight.setPower(rearRightPower);
+
+    }
+}
